fb2ba51588d5dcca6c33e8556ce3265ad7df986b
Neal Patel <nealajpatel@gmail.com>
2020-08-06 21:39:20 -0400

    modifying rm step in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,12 @@ fi
 # Initialize target with currently deployed files
 git clone --depth 1 --branch=master https://github.com/nealajpatel/code-trails-blog.git .deploy_git
 
-cd .deploy_git
+rm -rf .deploy_git/
+#cd .deploy_git
 
 # Remove all files before they get copied from ../public/
 # so git can track files that were removed in the last commit
-find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+#find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 
 cd ../
 
